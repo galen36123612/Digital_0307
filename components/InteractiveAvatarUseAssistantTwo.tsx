6c2855417935d1762841aa33adbce5dbb7fadbaa
galen36123612 <82554245+galen36123612@users.noreply.github.com>
2025-03-07 15:02:25 +0800

    Update InteractiveAvatarUseAssistantTwo.tsx

--- components/InteractiveAvatarUseAssistantTwo.tsx
+++ components/InteractiveAvatarUseAssistantTwo.tsx
@@ -15,9 +15,9 @@ import MicrophoneInput, { MicrophoneStatus } from "./MicrophoneInput";
 import { PauseCircle } from "@phosphor-icons/react/dist/ssr";
 
 //60439e8c0fe7428bb9b6c41772258a6b
-const avatarId = "cfbdfe9fd9de4307b81719fcbc6084c0"; //'Angela-insuit-20220820';
+const avatarId = "ed51e110ff96491c8595cf9c9c3f6d61"; //'Angela-insuit-20220820';
 //const avatarId = "52f3786c8c9543248a5cfcddad53813a"
-const voiceId = "dbb805f1b63a40ec869c66819ade215e";
+const voiceId = "45350457fa7248e081ca2cb64dec5e9f";
 
 export default function InteractiveAvatar() {
   const [isLoadingSession, setIsLoadingSession] = useState(false);
