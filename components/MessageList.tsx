57910c91b7565adfc22f627929ec1d5bc6e29ed1
galen36123612 <82554245+galen36123612@users.noreply.github.com>
2025-03-19 15:59:35 +0800

    Update MessageList.tsx

--- components/MessageList.tsx
+++ components/MessageList.tsx
@@ -15,7 +15,7 @@ export default function MessageList({ messages }: MessageListProps) {
     <div ref={view} className="w-full border-none flex flex-col gap-1">
       {messages.map((message: Message) => (
         <div key={message.id} className="flex flex-row ">
-          {//<span className="w-16 after:content-[':']">{message.role}</span> }
+          {// <span className="w-16 after:content-[':']">{message.role}</span> }
           <span className="flex-1 backdrop-blur-sm bg-white/10 rounded-md text-lg p-1 ">
             {message.content}
           </span>
@@ -24,4 +24,6 @@ export default function MessageList({ messages }: MessageListProps) {
     </div>
   );
 }
+
+
 
